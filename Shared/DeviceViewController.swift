f7264753f10e52abdb61a0fde1e4df961f409c0f
alimarie <alimarie205@gmail.com>
2017-08-25 15:35:57 +0200

    Status 25.08.17
    Start/Stop Monitoring functions created.
    Does not download or plot functions.
    Logs data.

--- Shared/DeviceViewController.swift
+++ Shared/DeviceViewController.swift
@@ -13,6 +13,11 @@ class DeviceViewController: UIViewController {
     
     // LABELS
     @IBOutlet weak var deviceStatus: UILabel!
+    @IBOutlet weak var TemperatureLabel: UILabel!
+    @IBOutlet weak var LightLabel: UILabel!
+    @IBOutlet weak var HumidityLabel: UILabel!
+    
+    
     
     // BUTTONS
     @IBOutlet weak var StartMonitoringButton: UIButton!
@@ -67,4 +72,90 @@ class DeviceViewController: UIViewController {
             }
         }
     }
+    
+    
+    //**********************************************************
+    //              UPDATING SETTINGS FUNCTIONS
+    //**********************************************************
+    
+    func updateAccelerometerBMI160Settings() {
+        let accelerometerBMI160 = self.device.accelerometer as! MBLAccelerometerBMI160
+        
+        accelerometerBMI160.fullScaleRange = .range2G
+        //self.accelerometerGraphView.fullScale = 2
+        accelerometerBMI160.sampleFrequency = 50
+    }
+    
+    
+    
+    
+    //**********************************************************
+    //                   TRACKING FUNCTIONS
+    //**********************************************************
+    
+    @IBAction func StartMonitoring(_ sender: Any) {
+        print("Start Monitoring tapped")
+        
+        StartMonitoringButton.isEnabled = false
+        StopMonitoringButton.isEnabled = true
+        device.led?.flashColorAsync(UIColor.magenta, withIntensity: 1.0)
+            
+            // ----- Track Movement -----
+            updateAccelerometerBMI160Settings()
+            device.accelerometer!.dataReadyEvent.startLoggingAsync()
+    }
+    
+    @IBAction func StopMonitoring(_ sender: Any) {
+        print("Stop Monitoring tapped")
+            
+        StartMonitoringButton.isEnabled = true
+        StopMonitoringButton.isEnabled = false
+        device.led?.setLEDOnAsync(false, withOptions: 1)
+        
+        //device.accelerometer!.dataReadyEvent.stopLoggingAsync()
+       
+      
+        
+            //-----  ACCELEROMETER -----
+  /*          let hud = MBProgressHUD.showAdded(to: UIApplication.shared.keyWindow!, animated: true)
+            hud.mode = .determinateHorizontalBar
+            hud.label.text = "Downloading..."
+            device.accelerometer!.dataReadyEvent.downloadLogAndStopLoggingAsync(true) { number in
+                hud.progress = number
+                }.success { array in
+                    self.accelerometerBMI160Data = array as! [MBLAccelerometerData]
+                    for obj in self.accelerometerBMI160Data {
+                        self.accelerometerGraphView.addX(obj.x, y: obj.y, z: obj.z)
+                    }
+                    hud.mode = .indeterminate
+                    hud.label.text = "Clearing Log..."
+                    self.logCleanup { error in
+                        hud.hide(animated: true)
+                        if error != nil {
+                            self.connectDevice(false)
+                        }
+                    }
+                }.failure { error in
+                    self.connectDevice(false)
+                    hud.hide(animated: true)
+            }
+            
+     
+*/
+        
+        
+    }
+    
+    
+    @IBAction func StartStreaming(_ sender: Any) {
+        print("Start Streaming tapped")
+    }
+    
+    @IBAction func StopStreaming(_ sender: Any) {
+        print("Stop Streaming tapped")
+    }
+    
+    
+    
+    
 }
